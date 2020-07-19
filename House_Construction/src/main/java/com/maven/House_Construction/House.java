c4b7000b270a63415ff2e5903afa2188bcd25671
ShivaKeerthi0 <60295265+ShivaKeerthi0@users.noreply.github.com>
2020-07-19 14:39:08 +0530

    Update House.java

--- House_Construction/src/main/java/com/maven/House_Construction/House.java
+++ House_Construction/src/main/java/com/maven/House_Construction/House.java
@@ -1,38 +1,30 @@
 package com.maven.House_Construction;
-
 import java.util.*;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-
-/**
- * Hello world!
- *
- */
 class HouseConstructionCost{
-    public void cost(){
+    public void ccost(){
         
-       Logger LOGGER =LogManager.getLogger(HouseConstructionCost.class);
+       Logger LOG =LogManager.getLogger(HouseConstructionCost.class);
 		Scanner sc= new Scanner(System.in);
-     LOGGER.info("select the type of material from the following");
-     LOGGER.info("\n 1.Standard Materials \n 2.Above Standard Materials \n 3.High Standard Materials \n 4.High Standard Materials and Fully Automated Home");
+     LOG.info("select the type of material from the following");
+     LOG.info("\n 1.Standard Materials \n 2.Above Standard Materials \n 3.High Standard Materials \n 4.High Standard Materials and Fully Automated Home");
         int n= sc.nextInt();
         
         switch(n){
-            case 1: LOGGER.info("the cost of the house Construction is 1200INR");break;
-            case 2: LOGGER.info("the cost of the house Construction is 1500INR");break;
-            case 3: LOGGER.info("the cost of the house Construction is 1800INR");break;
-            case 4: LOGGER.info("the cost of the house Construction is 2500INR");break;
-            default: LOGGER.error("enter the valid option");break;
+            case 1: LOG.info("the cost of the house Construction is 1200INR");break;
+            case 2: LOG.info("the cost of the house Construction is 1500INR");break;
+            case 3: LOG.info("the cost of the house Construction is 1800INR");break;
+            case 4: LOG.info("the cost of the house Construction is 2500INR");break;
+            default: LOG.error("enter the valid option");break;
                     }
         sc.close();
     }
 }
 public class House{
     public static void main(String args[]){
-        HouseConstructionCost hc=new HouseConstructionCost();
-        hc.cost();
+        HouseConstructionCost h=new HouseConstructionCost();
+        h.ccost();
 
     }
 }

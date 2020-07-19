68ae49c40e1470408ba40db03bf6dd682da39e97
ShivaKeerthi0 <60295265+ShivaKeerthi0@users.noreply.github.com>
2020-07-19 14:43:45 +0530

    Update Interest.java

--- Interest/src/main/java/com/maven/Interest/Interest.java
+++ Interest/src/main/java/com/maven/Interest/Interest.java
@@ -1,30 +1,24 @@
 package com.maven.Interest;
-import java.util .*;
-
+import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-/**
- * Hello world!
- *
- */
-
 public class Interest
 {
-   private static Logger LOGGER =LogManager.getLogger(Interest.class);
+   private static Logger LOG =LogManager.getLogger(Interest.class);
     public static void main (String args[ ])
 {
-    double p, rate, t, SimpleInterest,CompoundInterest;
+    double principle, rate, time, SimpleInterest,CompoundInterest;
     Scanner sc=new Scanner(System. in);
-    LOGGER.info("Enter the amount:");
-    p=sc.nextDouble();
-    LOGGER.info("Enter the No.of years:");
-    t=sc.nextDouble();
-    LOGGER.info("Enter the Rate of  interest");
+    LOG.info("Enter the principle:");
+    principle=sc.nextDouble();
+    LOG.info("Enter the time:");
+    time=sc.nextDouble();
+    LOG.info("Enter the Rate of Interest");
     rate=sc.nextDouble();
-    SimpleInterest=(p * t * rate)/100;
-    CompoundInterest =p * Math.pow(1.0+rate/100.0,t) - p;
-    LOGGER.info("Simple Interest="+SimpleInterest);
-    LOGGER.info("Compound Interest="+CompoundInterest);
+    SimpleInterest=(principle * time * rate)/100;
+    CompoundInterest =principle * Math.pow(1.0+rate/100.0,time) - principle;
+    LOG.info("Simple Interest="+SimpleInterest);
+    LOG.info("Compound Interest="+CompoundInterest);
     sc.close();
    }
     
